865e8092271a6c1348024343396a3813eee1f995
digiweb202 <digiwebnjoygroup@gmail.com>
2024-03-14 16:11:14 +0530

    14-03-24

--- lib/src/view/screen/product_list_screen.dart
+++ lib/src/view/screen/product_list_screen.dart
@@ -173,7 +173,7 @@ class ProductListScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "Hello Sina",
+                  "Hello Somil",
                   style: Theme.of(context).textTheme.displayLarge,
                 ),
                 Text(
